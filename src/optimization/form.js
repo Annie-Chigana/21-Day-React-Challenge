efe442051751911d179f5e87778cc64242a941a2
Annie-Chigana <chiganagana.a@gmail.com>
2025-01-10 21:10:20 +0200

    2nd project (optimization)

---
+++ src/optimization/form.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function Form() {
+    const [formData, setFormData] = useState({ name: "", email: ""});
+    const [errors, setErrors] = useState({});
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({...prevData, [name]: value }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!formData.name || !formData.email) {
+            setErrors({ message: "All fields are required!"});
+        } else {
+            setErrors({});
+            console.log("Form submitted:", formData);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                name="name"
+                value={formData.name}
+            />
+        </form>
+    )
+}
